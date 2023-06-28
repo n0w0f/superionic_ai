c1687900ee1bf324c3137312d1dab474bbace186
nawaf <iamnawafa@gmail.com>
2023-06-28 16:40:28 +0530

    [FIX] take loop out of workflow so that can be parallalized later

--- src/main.py
+++ src/main.py
@@ -34,54 +34,47 @@ class material_class:
     diffusion_coefficient: float
 
 
-def workflow(cif_files: List[str], config: dict) ->List[material_class]:
-    processed_save_path = config['data']['path']['processed_save_path']
-    relaxed_save_path = config['data']['path']['relaxed_save_path']
 
-    # formation_energy_cutoff = config['workflow']['screen_cutoffs']['formation_energy']
-    # bandgap_cutoff = config['workflow']['screen_cutoffs']['bandgap']
+def workflow(cif_file : str,  config: dict ) -> material_class:
 
-    workflow_results: List[material_class] = []
 
-    # Iterate over processed CIFs
-    for cif_file in cif_files:
-        updated_cif_filename = cif_file.replace("_updated.cif", "_relaxed.cif")
-        relaxed_cif_filename = updated_cif_filename.replace(processed_save_path, relaxed_save_path)
+    updated_cif_filename = cif_file.replace("_updated.cif", "_relaxed.cif")
+    relaxed_cif_filename = updated_cif_filename.replace(config['data']['path']['processed_save_path'], config['data']['path']['relaxed_save_path'])
 
-        # Read the unrelaxed structure from the processed CIF
-        unrelaxed_structure = Structure.from_file(cif_file)
+    # Read the unrelaxed structure from the processed CIF
+    unrelaxed_structure = Structure.from_file(cif_file)
 
-        # Perform geometry optimization (relaxation) on the unrelaxed structure
-        relaxed_structure = run_relax(unrelaxed_structure, config['model']['m3gnet']['relaxer'])
+    # Perform geometry optimization (relaxation) on the unrelaxed structure
+    relaxed_structure = run_relax(unrelaxed_structure, config['model']['m3gnet']['relaxer'])
 
-        cif_writer = CifWriter(relaxed_structure)
-        cif_writer.write_file(relaxed_cif_filename)
+    cif_writer = CifWriter(relaxed_structure)
+    cif_writer.write_file(relaxed_cif_filename)
 
-        # Perform formation energy prediction on the relaxed structure
-        formation_energy = predict_formation_energy(relaxed_structure, config['model']['m3gnet'])
+    # Perform formation energy prediction on the relaxed structure
+    formation_energy = predict_formation_energy(relaxed_structure, config['model']['m3gnet'])
 
-        stability = check_stability(formation_energy, config['workflow']['screen_cutoffs']['formation_energy'])
+    stability = check_stability(formation_energy, config['workflow']['screen_cutoffs']['formation_energy'])
 
-        # Perform bandgap prediction on the relaxed structure
-        bandgap = predict_bandgap(relaxed_structure, config['model']['m3gnet'])
 
-        insulator = check_conductivity(bandgap, config['workflow']['screen_cutoffs']['bandgap'])
+    # Perform bandgap prediction on the relaxed structure
+    bandgap = predict_bandgap(relaxed_structure, config['model']['m3gnet'])
 
-        # Create instances of WorkflowResult data class
-        relaxed_result = material_class(relaxed_cif_path=relaxed_cif_filename,
-                                        unrelaxed_cif_path=cif_file,
-                                        formation_energy=formation_energy,
-                                        bandgap=bandgap,
-                                        stable=stability,
-                                        insulator=insulator,
-                                        diffusion_coefficient=0,
-                                        )
+    insulator = check_conductivity(bandgap, config['workflow']['screen_cutoffs']['bandgap'])
 
-        # Append results to the respective lists
-        workflow_results.append(relaxed_result)
-    
+    # Create instances of WorkflowResult data class
+    relaxed_result = material_class(relaxed_cif_path=relaxed_cif_filename,
+                                    unrelaxed_cif_path=cif_file,
+                                    formation_energy=formation_energy,
+                                    bandgap=bandgap,
+                                    stable=stability,
+                                    insulator=insulator,
+                                    diffusion_coefficient=0,
+                                    )
 
-    return workflow_results
+    return relaxed_result 
+
+
+
 
 
 
@@ -92,12 +85,18 @@ if __name__ == "__main__":
     materials,substituted_materials = prepare_folders(config_data['data']['path'] , config_data['data']['substitution'] )
 
     raw_cif_paths , substituted_cif_paths = substitute_materials(materials,substituted_materials,config_data['data']['path'] , config_data['data']['substitution'])
+
     print(raw_cif_paths)
     print(substituted_cif_paths)
 
-    workflow_results =  workflow(substituted_cif_paths, config_data)
 
-    save_dataclass_list_to_json(workflow_results, "/home/nawaf/workflows/superionic_ai/src/data/json/data.json")
+    results: List[material_class] = []
+    for cif_file in substituted_cif_paths:
+
+        workflow_result =  workflow(substituted_cif_paths, config_data)
+        results.append(workflow_result)
+
+    save_dataclass_list_to_json(results, config_data['data']['path']['results'])
 
 
 
